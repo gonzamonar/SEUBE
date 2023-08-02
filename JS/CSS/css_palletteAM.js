2f32e8ad63f622c0fec1bb27894b24dac4ee2181
gonzamonar <98989353+gonzamonar@users.noreply.github.com>
2023-08-02 17:32:37 -0300

    Add files via upload

---
+++ JS/CSS/css_palletteAM.js
@@ -0,0 +1,5 @@
+var link = document.createElement( "link" );
+link.href = "https://gonzamonar.github.io/SEUBE/CSS/Palettes/palette_AM.css";
+link.type = "text/css";
+link.rel = "stylesheet";
+document.getElementsByTagName( "head" )[0].appendChild( link );
