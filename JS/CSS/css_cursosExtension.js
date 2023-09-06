73510d1de20b4f4635a76fa472a50887be356526
gonzamonar <98989353+gonzamonar@users.noreply.github.com>
2023-09-06 12:27:26 -0300

    Add files via upload

---
+++ JS/CSS/css_cursosExtension.js
@@ -0,0 +1,5 @@
+var link = document.createElement( "link" );
+link.href = "https://gonzamonar.github.io/SEUBE/CSS/cursos_extension.css";
+link.type = "text/css";
+link.rel = "stylesheet";
+document.getElementsByTagName( "head" )[0].appendChild( link );
