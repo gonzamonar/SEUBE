bf6aebfd39ce93ec8f327ddb3f5173c97f62a68c
gonzamonar <98989353+gonzamonar@users.noreply.github.com>
2023-09-06 12:39:47 -0300

    Update css_cursosExtension.js

--- JS/CSS/css_cursosExtension.js
+++ JS/CSS/css_cursosExtension.js
@@ -1,5 +1,3 @@
-var link = document.createElement( "link" );
-link.href = "https://gonzamonar.github.io/SEUBE/CSS/cursos_extension.css";
-link.type = "text/css";
-link.rel = "stylesheet";
-document.getElementsByTagName( "head" )[0].appendChild( link );
+import { appendCSS } from "https://gonzamonar.github.io/SEUBE/JS/Export/appendCSS.js";
+
+appendCSS("https://gonzamonar.github.io/SEUBE/CSS/cursos_extension.css");
