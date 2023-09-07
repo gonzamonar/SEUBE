2e2b63deb69070df744e4cd40ba822735611356f
gonzamonar <98989353+gonzamonar@users.noreply.github.com>
2023-09-07 16:15:45 -0300

    Update cursos_extension_2C_2023.js

--- JSON/cursos_extension_2C_2023.js
+++ JSON/cursos_extension_2C_2023.js
@@ -1,5 +1,6 @@
-import { CargarCursos } from "https://gonzamonar.github.io/SEUBE/JS/cursos_extension.js";
+import { CargarCursos, $, ApplyFilter } from "https://gonzamonar.github.io/SEUBE/JS/cursos_extension.js";
 import { appendCSS } from "https://gonzamonar.github.io/SEUBE/JS/Export/appendCSS.js";
 
 appendCSS("https://gonzamonar.github.io/SEUBE/CSS/cursos_extension.css");
 CargarCursos("https://gonzamonar.github.io/SEUBE/JSON/cursos_extension_2C_2023.json");
+$("").addEventListener("change", "ApplyFilter);
