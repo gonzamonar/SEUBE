126a4820b305288cd3e791be8cc73bbdbfba9d6b
gonzamonar <98989353+gonzamonar@users.noreply.github.com>
2024-04-05 14:36:22 -0300

    Se agregan los Cursos de Extensión del 1°C 2024.

---
+++ JSON/cursos_extension_1C_2024.js
@@ -0,0 +1,5 @@
+import { CargarCursos } from "https://gonzamonar.github.io/SEUBE/JS/cursos_extension.js";
+import { appendCSS } from "https://gonzamonar.github.io/SEUBE/JS/Export/appendCSS.js";
+
+appendCSS("https://gonzamonar.github.io/SEUBE/CSS/cursos_extension.css");
+CargarCursos("https://gonzamonar.github.io/SEUBE/JSON/cursos_extension_1C_2024.json", true);
