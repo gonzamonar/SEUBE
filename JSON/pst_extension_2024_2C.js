f77d4f99e7db6d508c93d8602ac437ee5a2dc11f
Gonza Monar <98989353+gonzamonar@users.noreply.github.com>
2024-07-26 17:14:43 -0300

    Add files via upload

---
+++ JSON/pst_extension_2024_2C.js
@@ -0,0 +1,5 @@
+import { CargarCursos } from "https://gonzamonar.github.io/SEUBE/JS/pst_extension.js";
+import { appendCSS } from "https://gonzamonar.github.io/SEUBE/JS/Export/appendCSS.js";
+
+appendCSS("https://gonzamonar.github.io/SEUBE/CSS/pst_extension.css");
+CargarCursos("https://gonzamonar.github.io/SEUBE/JSON/pst_extension_2024_2C.json");
