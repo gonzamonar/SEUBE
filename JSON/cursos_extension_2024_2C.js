1ff4893a910b76ec94432a363088c34505a6029b
Gonza Monar <98989353+gonzamonar@users.noreply.github.com>
2024-07-29 13:18:49 -0300

    Create cursos_extension_2024_2C.js

---
+++ JSON/cursos_extension_2024_2C.js
@@ -0,0 +1,5 @@
+import { CargarCursos } from "https://gonzamonar.github.io/SEUBE/JS/cursos_extension_v2.js";
+import { appendCSS } from "https://gonzamonar.github.io/SEUBE/JS/Export/appendCSS.js";
+
+appendCSS("https://gonzamonar.github.io/SEUBE/CSS/cursos_extension_v2.css");
+CargarCursos("https://gonzamonar.github.io/SEUBE/JSON/cursos_extension_2024_2C.json", true);
