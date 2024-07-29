0da0129d1e5ba32bcdfef7643a10bb48d991302b
Gonza Monar <98989353+gonzamonar@users.noreply.github.com>
2024-07-29 13:24:02 -0300

    Update and rename cursos_extension_verano_2024.js to cursos_extension_2024_verano.js

--- JSON/cursos_extension_2024_verano.js
+++ JSON/cursos_extension_2024_verano.js
@@ -2,4 +2,4 @@ import { CargarCursos } from "https://gonzamonar.github.io/SEUBE/JS/cursos_exten
 import { appendCSS } from "https://gonzamonar.github.io/SEUBE/JS/Export/appendCSS.js";
 
 appendCSS("https://gonzamonar.github.io/SEUBE/CSS/cursos_extension.css");
-CargarCursos("https://gonzamonar.github.io/SEUBE/JSON/cursos_extension_verano_2024.json", true);
+CargarCursos("https://gonzamonar.github.io/SEUBE/JSON/cursos_extension_2024_verano.json", true);
