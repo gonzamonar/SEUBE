525716b90fc44505a64ee90bdfcadafcb30f06f0
Gonza Monar <98989353+gonzamonar@users.noreply.github.com>
2025-02-19 15:14:38 -0300

    Update pst_grado_2025_1C.js

--- JSON/pst_grado_2025_1C.js
+++ JSON/pst_grado_2025_1C.js
@@ -2,4 +2,4 @@ import { CargarCursos } from "https://gonzamonar.github.io/SEUBE/JS/pst_grado.js
 import { appendCSS } from "https://gonzamonar.github.io/SEUBE/JS/Export/appendCSS.js";
 
 appendCSS("https://gonzamonar.github.io/SEUBE/CSS/pst_grado.css");
-CargarCursos("https://gonzamonar.github.io/SEUBE/JSON/pst_grado_2025_1C.json", true);
+CargarCursos("https://gonzamonar.github.io/SEUBE/JSON/pst_grado_2025_1C.json");
