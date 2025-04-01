c335dd50508879c217b7fa30b3a327581cee092d
Gonza Monar <98989353+gonzamonar@users.noreply.github.com>
2025-04-01 17:04:54 -0300

    Add files via upload

---
+++ JSON/cursos_equipos_2025_1C.js
@@ -0,0 +1,5 @@
+import { CargarCursos } from "https://gonzamonar.github.io/SEUBE/JS/cursos_equipos_extension.js";
+import { appendCSS } from "https://gonzamonar.github.io/SEUBE/JS/Export/appendCSS.js";
+
+appendCSS("https://gonzamonar.github.io/SEUBE/CSS/cursos_equipos_extension.css");
+CargarCursos("https://gonzamonar.github.io/SEUBE/JSON/cursos_equipos_2025_1C.json");
