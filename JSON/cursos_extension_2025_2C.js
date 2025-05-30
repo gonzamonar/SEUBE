00e3a108ae852aa6b49f4f4cfa0c4dbc7da57413
Gonza Monar <98989353+gonzamonar@users.noreply.github.com>
2025-05-30 14:52:11 -0300

    Add files via upload

---
+++ JSON/cursos_extension_2025_2C.js
@@ -0,0 +1,5 @@
+import { CargarCursos } from "https://gonzamonar.github.io/SEUBE/JS/cursos_extension_v4.js";
+import { appendCSS } from "https://gonzamonar.github.io/SEUBE/JS/Export/appendCSS.js";
+
+appendCSS("https://gonzamonar.github.io/SEUBE/CSS/flyer_cursos_extension_v1.css");
+CargarCursos("https://gonzamonar.github.io/SEUBE/JSON/cursos_extension_2025_2C.json");
