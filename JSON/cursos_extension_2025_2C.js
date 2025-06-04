bfdc2afa7829fd570b11a6c04c09f78dd532658a
Gonza Monar <98989353+gonzamonar@users.noreply.github.com>
2025-06-04 15:54:30 -0300

    Update cursos_extension_2025_2C.js

--- JSON/cursos_extension_2025_2C.js
+++ JSON/cursos_extension_2025_2C.js
@@ -4,6 +4,6 @@ import { appendCSS } from "https://gonzamonar.github.io/SEUBE/JS/Export/appendCS
 const JSON_FILE = "https://gonzamonar.github.io/SEUBE/JSON/cursos_extension_2025_2C.json";
 
 appendCSS("https://gonzamonar.github.io/SEUBE/CSS/flyer_cursos_extension_v1.css");
-appendCSS("https://gonzamonar.github.io/SEUBE/CSS/cursos_extension_v2.css");
+appendCSS("https://gonzamonar.github.io/SEUBE/CSS/cursos_extension_v3.css");
 CargarCursos(JSON_FILE);
 CargarPagina(JSON_FILE);
