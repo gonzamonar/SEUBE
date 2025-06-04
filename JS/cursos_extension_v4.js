a4be47f8b6c59f5d733769c78cdb03e4405ed33c
Gonza Monar <98989353+gonzamonar@users.noreply.github.com>
2025-06-04 16:04:15 -0300

    Update cursos_extension_v4.js

--- JS/cursos_extension_v4.js
+++ JS/cursos_extension_v4.js
@@ -135,12 +135,19 @@ function renderCurso(curso) {
 
 function renderPagina(curso) {
     const modalidad = curso.modalidad.charAt(0).toUpperCase() + curso.modalidad.slice(1);
-    const inscripcion = curso.inscripcion.toUpperCase();
+    const isOpen = curso.inscripcion.toLowerCase() == "abierta";
+    const inscription_btn = isOpen
+	  ? `
+	      <p><a class="inscr_btn" href="${curso.link}" target="_blank">Inscribirse</a></p>
+	    `
+	  : `
+	      <p>Inscripción Cerrada</p>
+	    `;
 	
     return `
-        <div class="course_heading ${inscripcion == "CERRADA" ? "cerrada" : ""}">
+        <div class="course_heading ${curso.inscripcion.toLowerCase()}">
             <div class="title-container">
-                <p class="course_info">Curso N°${curso.n}<span class="course_info_slash">|</span> INSCRIPCIÓN ${inscripcion}</p>
+                <p class="course_info">Curso N°${curso.n}<span class="course_info_slash">|</span> INSCRIPCIÓN ${curso.inscripcion.toUpperCase()}</p>
                 <p class="course_title">${curso.titulo}</p>
 		<p class="course_subtitle">${curso.subtitulo}</p>
             </div>
@@ -203,9 +210,10 @@ function renderPagina(curso) {
                     <div class="fieldtitle field16">PRESENTACIÓN</div>
                     <div class="presentation">${curso.presentacion}</div>
                 </div>
-                <div class="btn_container">
-                    <p><a class="inscr_btn" href="${curso.link}" target="_blank">Inscribirse</a></p>
-                </div>
+		<div class="btn_container">
+  		${inscription_btn}
+    		</div>
+		
             </div>
         </div>
     `;
