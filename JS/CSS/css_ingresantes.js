34208fc74947fb553db535677622f7d4ee010674
Gonza Monar <98989353+gonzamonar@users.noreply.github.com>
2025-06-10 12:38:42 -0300

    Add files via upload

---
+++ JS/CSS/css_ingresantes.js
@@ -0,0 +1,5 @@
+var link = document.createElement( "link" );
+link.href = "https://gonzamonar.github.io/SEUBE/CSS/ingresantes.css";
+link.type = "text/css";
+link.rel = "stylesheet";
+document.getElementsByTagName( "head" )[0].appendChild( link );
