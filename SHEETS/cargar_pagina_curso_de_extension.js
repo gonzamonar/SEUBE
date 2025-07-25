56500a7a61812b7dc2985f46e562464632e45c7b
Gonza Monar <98989353+gonzamonar@users.noreply.github.com>
2025-07-25 16:33:18 -0300

    Create cargar_pagina_curso_de_extension.js

---
+++ SHEETS/cargar_pagina_curso_de_extension.js
@@ -0,0 +1,5 @@
+import { CargarPagina } from "https://gonzamonar.github.io/SEUBE/JS/cursos_extension_v4.js";
+import { appendCSS } from "https://gonzamonar.github.io/SEUBE/JS/Export/appendCSS.js";
+
+appendCSS("https://gonzamonar.github.io/SEUBE/CSS/cursos_extension_v3.css");
+CargarPagina();
