f0fc6268b791726be812db521c2e1cb83d09038a
Gonza Monar <98989353+gonzamonar@users.noreply.github.com>
2025-08-22 17:11:05 -0300

    Add files via upload

---
+++ SHEETS/cargar_cursos_universitariosxunmes.js
@@ -0,0 +1,5 @@
+import { CargarCursos } from "https://gonzamonar.github.io/SEUBE/JS/cursos_universitariosxunmes.js";
+import { appendCSS } from "https://gonzamonar.github.io/SEUBE/JS/Export/appendCSS.js";
+
+appendCSS("https://gonzamonar.github.io/SEUBE/CSS/cursos_universitariosxunmes.css");
+CargarCursos();
