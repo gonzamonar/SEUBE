19785c19c0bb60ff3881f5a80c2f49980ad75c69
Gonza Monar <98989353+gonzamonar@users.noreply.github.com>
2025-11-24 22:07:33 -0300

    Update and rename cargar_seminarios_pst.js to cargar_seminarios_pst_grado.js

--- SHEETS/cargar_seminarios_pst.js
+++
@@ -1,5 +0,0 @@
-import { CargarCursos } from "https://gonzamonar.github.io/SEUBE/JS/SHEETS/seminarios_pst.js";
-import { appendCSS } from "https://gonzamonar.github.io/SEUBE/JS/Export/appendCSS.js";
-
-appendCSS("https://gonzamonar.github.io/SEUBE/CSS/SHEETS/seminarios_pst.css");
-CargarCursos();
---
+++ SHEETS/cargar_seminarios_pst_grado.js
@@ -0,0 +1,5 @@
+import { CargarCursos } from "https://gonzamonar.github.io/SEUBE/JS/SHEETS/seminarios_pst_grado.js";
+import { appendCSS } from "https://gonzamonar.github.io/SEUBE/JS/Export/appendCSS.js";
+
+appendCSS("https://gonzamonar.github.io/SEUBE/CSS/SHEETS/seminarios_pst_grado.css");
+CargarCursos();
