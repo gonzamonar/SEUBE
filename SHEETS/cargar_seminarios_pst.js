95622b4db1c587ac7024916293a9d67c6d3b2290
Gonza Monar <98989353+gonzamonar@users.noreply.github.com>
2025-11-24 21:52:51 -0300

    Update cargar_seminarios_pst.js

--- SHEETS/cargar_seminarios_pst.js
+++ SHEETS/cargar_seminarios_pst.js
@@ -1,5 +1,5 @@
-import { CargarCursos } from "https://gonzamonar.github.io/SEUBE/JS/seminarios_pst.js";
+import { CargarCursos } from "https://gonzamonar.github.io/SEUBE/JS/SHEETS/seminarios_pst.js";
 import { appendCSS } from "https://gonzamonar.github.io/SEUBE/JS/Export/appendCSS.js";
 
-appendCSS("https://gonzamonar.github.io/SEUBE/CSS/seminarios_pst.css");
+appendCSS("https://gonzamonar.github.io/SEUBE/CSS/SHEETS/seminarios_pst.css");
 CargarCursos();
