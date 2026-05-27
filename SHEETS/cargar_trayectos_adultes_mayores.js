a998e9933a00c041a04e7b1109bb4969cd4af972
Gonza Monar <98989353+gonzamonar@users.noreply.github.com>
2026-05-27 13:21:18 -0300

    Create cargar_trayectos_adultes_mayores.js

---
+++ SHEETS/cargar_trayectos_adultes_mayores.js
@@ -0,0 +1,5 @@
+import { CargarTrayectos } from "https://gonzamonar.github.io/SEUBE/JS/trayectos_am.js";
+import { appendCSS } from "https://gonzamonar.github.io/SEUBE/JS/Export/appendCSS.js";
+
+appendCSS("https://gonzamonar.github.io/SEUBE/CSS/trayectos_am.css");
+CargarTrayectos();
